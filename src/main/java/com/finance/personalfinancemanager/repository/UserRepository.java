de92e64d9ab5ac78c156be2f9d8abcbfd7b1153f
asmaicse <asmai.cse@gmail.com>
2026-06-04 11:45:12 +0530

    Created UserREpository

---
+++ src/main/java/com/finance/personalfinancemanager/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.finance.personalfinancemanager.repository;
+import com.finance.personalfinancemanager.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
